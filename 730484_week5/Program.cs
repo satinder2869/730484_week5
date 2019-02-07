53012ad5d1944506cd2347f5450ff7aea0980c2b
visualstudio <visualstudio@230-PC12>
2019-02-07 15:58:24 -0500

    4:00 pm

--- 730484_week5/Program.cs
+++ 730484_week5/Program.cs
@@ -1,62 +1,206 @@
 ﻿using System;
 
 namespace _730484_week5
+{ 
+class Program
+
 {
-    class Program
+
+    static void Main(string[] args)
+
     {
-        static void Main(string[] args)
-        {
-            var a = new testques2();
-            a.Play();
-        }
+
+        var a = new TestQuestion2();
+
+        //a.PlayingWithForLoops();
+
+
+
+        var b = new birthday_party();
+
+        b.setupPartyList();
+
+        b.printPartyList();
+
+
+
     }
 
-    class testques2
-    {
-        public int myfavvari = 0;
-        public void Play()
-        {
-            //write a for loop
+}
 
-            for (; my();)
+
+
+class TestQuestion2
+
+{
+
+    public int myFavoriteVariable = 0;
+
+
+
+    public void PlayingWithForLoops()
+
+    {
+
+        // write a For Loop to Add 10 Numbers
+
+        while (MyMethod())
+
+        {
+
+            if (myFavoriteVariable > 10)
+
             {
-                if (myfavvari <= 10)
-                    Console.WriteLine("you are fool");
+
+                Console.WriteLine("i am so out of here!");
+
                 break;
+
             }
-            Console.WriteLine("shit for loop again");
+
+            Console.WriteLine("oh no I have to go through this stupid loop again...");
+
         }
-        public bool my()
-        {
-            myfavvari++;
-            return true;
-        }
+
     }
 
-    class dog
+    public bool MyMethod()
+
     {
-        public dog(string name, string breed)
-        {
-            dog_name = name;
-            dog_breed = breed;
-        }
-        public string dog_name;
-        public string dog_breed;
 
-        public dog next dog;
-public dog prev_dog;
+        myFavoriteVariable++;
+
+        return true;
 
     }
-    class birthday_party
+
+}
+
+
+
+class dog
+
+{
+
+    public dog(string name, string breed)
+
     {
-        public dog peanut;
-        public dog fifi;
-        public dog clarence;
-        public dog roy;
 
-        public dog head;
-        public dog tail;
-        public dog temporary;
+        dog_name = name;
+
+        dog_breed = breed;
+
     }
+
+
+
+    public dog()
+
+    {
+
+    }
+
+
+
+    public string dog_name;
+
+    public string dog_breed;
+
+    public dog next_dog;
+
+    public dog prev_dog;
+
+
+
+}
+
+
+
+class birthday_party
+
+{
+
+    public dog peanut;
+
+    public dog fifi;
+
+    public dog clarence;
+
+    public dog roy;
+
+
+
+    public dog head;
+
+    public dog tail;
+
+    public dog temporary;
+
+
+
+    public void setupPartyList()
+
+    {
+
+        peanut = new dog("Peanut", "Bichon");
+
+        fifi = new dog("Fifi", "Poodle");
+
+        clarence = new dog("Clarence", "German Sheppard");
+
+        roy = new dog("Roy", "Beagle");
+
+
+
+        peanut.prev_dog = null;
+
+        peanut.next_dog = fifi;
+
+        fifi.prev_dog = peanut;
+
+        fifi.next_dog = clarence;
+
+        clarence.prev_dog = fifi;
+
+        clarence.next_dog = roy;
+
+        roy.prev_dog = clarence;
+
+        roy.next_dog = null;
+
+        head = peanut;
+
+        tail = roy;
+
+    }
+
+
+
+    public string printPartyList()
+
+    {
+
+        string inviteList = "*--";
+
+        temporary = head;
+
+        while (temporary.next_dog != null)
+
+        {
+
+            inviteList += temporary.dog_name + " * --- * ";
+
+        }
+
+
+
+        return inviteList;
+
+    }
+
+
+
+}
+
 }
 
